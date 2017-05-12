52512bc882e5b260ee9e07d6a9b2b6c23295ae18
user <thecodecoaster@gmail.com>
2017-05-12 12:09:33 -0400

    typed variables

--- 0-typescript/types.js
+++ 0-typescript/types.js
@@ -2,7 +2,14 @@
 var myString;
 var myNum;
 var myBool;
+var myVar;
+var stringArray;
+var numberArray;
 myString = 'Test 1,2,3,4,5,6,7,8,9!'.slice(0, 5);
 myNum = 5 % 9;
 myBool = true;
-console.log(myString + '\n' + myNum + '\n' + myBool + '\n');
+myVar = true;
+stringArray = ['Clef 1', 'Clef 2', 'Clef 3', 'Clef 4', 'Clef 5'];
+numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+document.write(myString + '<br />' + myNum + '<br />' + myBool + '<br />' + myVar + '<br />' +
+    stringArray + '<br />' + numberArray + '<br />');
--- 0-typescript/types.ts
+++ 0-typescript/types.ts
@@ -1,13 +1,24 @@
 //console.log('test');
 
-let myString: string
-let myNum: number
-let myBool: boolean
+var myString: string
+var myNum: number
+var myBool: boolean
+var myVar: any
+
+var stringArray: string[]
+var numberArray: number[]
 
 myString = 'Test 1,2,3,4,5,6,7,8,9!'.slice(0, 5)
 myNum = 5 % 9
 myBool = true
 
+myVar = true
 
-console.log(myString + '\n' + myNum + '\n' + myBool + '\n')
+stringArray = ['Clef 1', 'Clef 2', 'Clef 3', 'Clef 4', 'Clef 5']
+numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+document.write(myString + '<br />' + myNum + '<br />' + myBool + '<br />' + myVar + '<br />' +
+    stringArray + '<br />' + numberArray + '<br />'
+
+)
 
