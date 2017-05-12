453c5f4c093bff5e13b73df542f81f7fafeb395c
user <thecodecoaster@gmail.com>
2017-05-12 13:00:50 -0400

    functions

--- 0-typescript/functions.js
+++ 0-typescript/functions.js
@@ -1,5 +1,15 @@
 console.log('***FUNCTIONS*****');
 function getSum(num1, num2) {
-    return num1 + num2;
+    return num1 % num2;
 }
 console.log(getSum(Math.PI, Math.sqrt(100)));
+var mySum = function (num1, num2) {
+    if (typeof num1 == 'string') {
+        num1 = parseInt(num1);
+    }
+    if (typeof num2 == 'string') {
+        num2 = parseInt(num2);
+    }
+    return num1 + num2;
+};
+console.log(mySum(10, 5));
