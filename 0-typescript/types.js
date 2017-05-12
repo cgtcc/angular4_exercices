8c0ba51ca6f49c7076ba6773002de15bd333667c
user <thecodecoaster@gmail.com>
2017-05-12 11:56:47 -0400

    typed variables

---
+++ 0-typescript/types.js
@@ -0,0 +1,8 @@
+//console.log('test');
+var myString;
+var myNum;
+var myBool;
+myString = 'Test 1,2,3!\n';
+myNum = 55;
+myBool = true;
+console.log(myString + myNum + myBool);
---
+++ 0-typescript/types.ts
@@ -0,0 +1,13 @@
+//console.log('test');
+
+let myString: string
+let myNum: number
+let myBool: boolean
+
+myString = 'Test 1,2,3!\n'
+myNum = 55
+myBool = true
+
+
+console.log(myString + myNum + myBool)
+
