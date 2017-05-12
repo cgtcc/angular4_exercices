23fb8a3750f3ba4b2ff936468f0af8d6e0ff7184
user <thecodecoaster@gmail.com>
2017-05-12 12:00:28 -0400

    typed variables

--- 0-typescript/types.js
+++ 0-typescript/types.js
@@ -2,7 +2,7 @@
 var myString;
 var myNum;
 var myBool;
-myString = 'Test 1,2,3!\n';
-myNum = 55;
+myString = 'Test 1,2,3,4,5,6,7,8,9!'.slice(0, 5);
+myNum = 5 % 9;
 myBool = true;
-console.log(myString + myNum + myBool);
+console.log(myString + '\n' + myNum + '\n' + myBool + '\n');
--- 0-typescript/types.ts
+++ 0-typescript/types.ts
@@ -4,10 +4,10 @@ let myString: string
 let myNum: number
 let myBool: boolean
 
-myString = 'Test 1,2,3!\n'
-myNum = 55
+myString = 'Test 1,2,3,4,5,6,7,8,9!'.slice(0, 5)
+myNum = 5 % 9
 myBool = true
 
 
-console.log(myString + myNum + myBool)
+console.log(myString + '\n' + myNum + '\n' + myBool + '\n')
 
