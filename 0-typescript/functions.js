4f7a66b4ed2d205a91e9d2165afe1ed9c8c514d8
user <thecodecoaster@gmail.com>
2017-05-12 12:53:34 -0400

    functions

--- 0-typescript/functions.js
+++ 0-typescript/functions.js
@@ -2,4 +2,4 @@ console.log('***FUNCTIONS*****');
 function getSum(num1, num2) {
     return num1 + num2;
 }
-console.log(getSum(1, 4));
+console.log(getSum(Math.PI, Math.sqrt(100)));
--- 0-typescript/functions.ts
+++ 0-typescript/functions.ts
@@ -1,8 +1,8 @@
 
 console.log('***FUNCTIONS*****')
 
-function getSum(num1, num2) {
+function getSum(num1: number, num2: number): number {
     return num1 + num2;
 }
 
-console.log(getSum(1, 4))
+console.log(getSum(Math.PI, Math.sqrt(100)))
