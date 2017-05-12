9728e598932d9818dc4638edee932c64ecacd75d
user <thecodecoaster@gmail.com>
2017-05-12 12:49:50 -0400

    functions

--- 0-typescript/types.ts
+++ 0-typescript/types.ts
@@ -23,11 +23,11 @@ myString = 'Test 1,2,3,4,5,6,7,8,9!'.slice(0, 5)
 myNum = 5 % 9
 myBool = true
 myVar = true
-stringArray = ['Clef 1', 'Clef 2', 'Clef 3', 'Clef 4', 'Clef 5']
-numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+stringArray = ['Clef 1', 'Clef 2', 'Clef 3', 'Clef 4', 'Clef 5'].reverse()
+numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, Math.sqrt(16)]
 booleanArray = [true, true, false, false, true]
 
-strNumTuple = ['Clef 1', myString, 1000, 100, myNum, myNum]
+strNumTuple = ['Clef 1', myString, 1000, Math.PI, myNum, myNum]
 
 var myUndefined: undefined = null
 var myVoid: void = undefined
