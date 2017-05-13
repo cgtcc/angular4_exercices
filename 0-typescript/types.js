e785c86905ed8b1a37b37d68d56a7355399ddb05
user <thecodecoaster@gmail.com>
2017-05-13 00:28:47 -0400

    let for variables

--- 0-typescript/types.js
+++ 0-typescript/types.js
@@ -5,9 +5,9 @@ var myNum;
 var myBool;
 var myVar;
 /*
-var stringArray: string[]
-var numberArray: number[]
-var booleanArray: boolean[]
+let stringArray: string[]
+let numberArray: number[]
+let booleanArray: boolean[]
 */
 var stringArray;
 var numberArray;
@@ -17,7 +17,7 @@ var strNumTuple;
 myString = 'Test 1,2,3,4,5,6,7,8,9!'.slice(0, 5);
 myNum = 5 % 9;
 myBool = true;
-myVar = true;
+mylet = true;
 stringArray = ['Clef 1', 'Clef 2', 'Clef 3', 'Clef 4', 'Clef 5'].reverse();
 numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, Math.sqrt(16)];
 booleanArray = [true, true, false, false, true];
--- 0-typescript/types.ts
+++ 0-typescript/types.ts
@@ -2,36 +2,36 @@ console.log('***TYPED VARIABLES*****')
 
 //typed variables declaration
 
-var myString: string
-var myNum: number
-var myBool: boolean
-var myVar: any
+let myString: string
+let myNum: number
+let myBool: boolean
+let myVar: any
 /*
-var stringArray: string[]
-var numberArray: number[]
-var booleanArray: boolean[]
+let stringArray: string[]
+let numberArray: number[]
+let booleanArray: boolean[]
 */
-var stringArray: Array<string>
-var numberArray: Array<number>
-var booleanArray: Array<boolean>
+let stringArray: Array<string>
+let numberArray: Array<number>
+let booleanArray: Array<boolean>
 
-var strNumTuple: [string, string, number]
+let strNumTuple: [string, string, number]
 
 //assign values
 
 myString = 'Test 1,2,3,4,5,6,7,8,9!'.slice(0, 5)
 myNum = 5 % 9
 myBool = true
-myVar = true
+mylet = true
 stringArray = ['Clef 1', 'Clef 2', 'Clef 3', 'Clef 4', 'Clef 5'].reverse()
 numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, Math.sqrt(16)]
 booleanArray = [true, true, false, false, true]
 
 strNumTuple = ['Clef 1', myString, 1000, Math.PI, myNum, myNum]
 
-var myUndefined: undefined = null
-var myVoid: void = undefined
-var myNull: null = void
+let myUndefined: undefined = null
+let myVoid: void = undefined
+let myNull: null = void
 
     //output
 
